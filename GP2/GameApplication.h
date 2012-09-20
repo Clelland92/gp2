d7acff715fa5bc01c5e56404f8546409d24fa65a
Marc Clelland <mclell200@caledonian.ac.uk>
2012-09-20 11:51:52 +0000

    Added functions in the required areas

--- GP2/GameApplication.h
+++ GP2/GameApplication.h
@@ -13,13 +13,14 @@ public:
 	bool init();
 	void run();
 private:
+	bool initGame();
 	bool initGraphics();
 	bool initWindow();
 
 	void render();
 	void update();
 private:
-	ID3D10Device*m_pD3D10DEVICE;
+	ID3D10Device*m_pD3D10Device;
 	IDXGISwapChain*m_pSwapChain;
 	ID3D10RenderTargetView*m_pRenderTargetView;
 	CWin32Window*m_pWindow;
