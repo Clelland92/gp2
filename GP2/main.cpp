b28425b2cddbabce6b380f2e6187fdc0072f6a75
Marc Clelland <mclell200@caledonian.ac.uk>
2012-10-03 15:53:17 +0100

    finsihed adding the required functions

--- GP2/main.cpp
+++ GP2/main.cpp
@@ -2,38 +2,30 @@
 #ifdef WIN32
 //if so include the header for windows applications
 #include <Windows.h>
-#include "GameApplication.h"
 #endif
 
 #include "Win32Window.h"
 
 //Check to see if we are on windows
 #ifdef WIN32
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
-{
-	CGameApplication *pApp=new CGameApplication();
-	if(!pApp->init())
-	{
-		if(pApp)
-		{
-			delete pApp;
-			pApp=NULL;
-			return 1;
-		}
-	}
-
-	pApp->run();
-
-	if(pApp)
-	{
-		delete pApp;
-		pApp=NULL;
-	}
-	return 0;
-}
+int WINAPI WinMain(HINSTANCE hInstance, 
+					HINSTANCE hPrevInstance, 
+					LPSTR lpCmdLine,
+					int nCmdShow)
 #else
 //we are on some other platform
 int main(int argc, char **argv)
 #endif
+{
+	CWin32Window * pWin=new CWin32Window();
+	pWin->init(TEXT("Marc"),860,640,false);
 
+	while(pWin->running()){
+		pWin->checkForWindowMessages();
+	}
+
+	delete pWin;
+
+	return 0;
+}
 
